8aa6f3afdda15c7370fbc88e69d9503a84c14543
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2023-09-11 20:02:24 -0400

    running FizzBuzz.doFizzBuzz(), which is written in another file.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int k = 1;
+        while (k < 100) {
+            FizzBuzz.doFizzBuzz(k);
+            k++;
+        }
+
+    }
+}
